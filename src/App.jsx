3d99713be6f08ad8c0b502724bbdd071daf06c3e
sanc0o0 <sanansari0305@gmail.com>
2026-03-09 22:17:52 +0530

    style: add global background grid and fix layout for full height

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import Navbar from './components/Navbar'
-import './App.css'
+import Navbar from "./components/Navbar";
+import Manager from "./components/Manager";
+import Footer from "./components/Footer";
+import SavedCredentials from "./components/SavedCredentials";
+import  { Toaster } from "react-hot-toast";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <>
-    <Navbar/>
-      
+      <Toaster position="top-right" />
+
+      <div className=" min-h-screen flex flex-col ">
+      {/* <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-size-[6rem_4rem]">
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div>
+      </div> */}
+        <Navbar />
+
+        <main className="grow">
+          <Routes>
+            <Route path="/" element={<Manager />} />
+            <Route path="/saved" element={<SavedCredentials />} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
